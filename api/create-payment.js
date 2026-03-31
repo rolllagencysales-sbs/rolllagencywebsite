2046b18c26bd64b6eeb9596ed15fa01d5c38d9ec
Kerem Bülbül <kerembulbul1@outlook.com>
2026-03-31 05:50:48 +0300

    Siteyi güncelledim

--- api/create-payment.js
+++ api/create-payment.js
@@ -50,6 +50,7 @@ module.exports = async function handler(req, res) {
     title: safeProductName,
     description: `${safeProductName} için özel ödeme bağlantısı`,
     status: "active",
+    customListing: false,
     type: "digital",
     deliveryType: "digital",
     shippingPayer: "sellerPays",
